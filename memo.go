989cb751f5ff2d7288641b8565d58de5c872444d
jucky154 <chemistry54@docomo.ne.jp>
2020-12-06 20:45:28 +0900

    shake hands and send log

--- memo.go
+++ memo.go
@@ -1,31 +1,94 @@
+/*
+ implements the real time contest client software.
+ Copyright (C) 2020 JA1ZLO.
+ */
 package main
 
-import (
-		"log"
-		"time"
-		"strings"
-		"golang.org/x/net/websocket"
+import "C"
+import "github.com/nextzlog/zylo"
+import "log"
+import "os"
+import "unsafe"
+import "golang.org/x/net/websocket"
+import "time"
+
+var(
+	url="wss://realtime.allja1.org/agent/JA1ZLO/a6ceda1d-517a-404b-92d5-2b7bfb9bc73b"
+	origin="https://realtime.allja1.org/agent/JA1ZLO/"
 )
 
-	var (
-		origin = "http://localhost:9000/"
-		url = "ws://localhost:9000/flash"
-)
-
-type EchoMsg struct{
-	zlog unit64
+func pointer_to_qso(ptr uintptr) *zylo.QSO {
+	return (*zylo.QSO)(unsafe.Pointer(ptr))
 }
 
-func main(){
-	ws, err := websocket.Dial("url", "", "origin");
- 	if err != nil {
-		panic("Dial: " + err.String())
+//export zlaunch
+func zlaunch(uintptr) {
+	ws, err := websocket.Dial(url,"",origin)
+	_=ws.Close()
+	
+	if err != nil{	
+		file, err := os.Create("output.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		file.WriteString("No")
+
+		if err := file.Close(); err != nil {
+			log.Fatal(err)
+		}
 	}
-	if _, err := ws.Write(zlog); err != nil {
-		panic("Write: " + err.String())
-	}
-	var msg = make([]byte, 512);
-	if n, err := ws.Read(msg); err != nil {
-		panic("Read: " + err.String())
+	if err == nil{	
+		file, err := os.Create("output.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		file.WriteString("Yes")
+
+		if err := file.Close(); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
+
+//export zrevise
+func zrevise(ptr uintptr) {
+	qso := zylo.ToQSO(ptr)
+	qso.SetMul1(qso.GetRcvd())
+}
+
+//export zverify
+func zverify(ptr uintptr) (score int) {
+	score = 0;
+	return;
+}
+
+//export zresult
+func zresult(qso uintptr) (total int) {
+	total = 0;
+	return;
+}
+
+//export zinsert
+func zinsert(ptr uintptr) {
+	insert := []byte{0}
+	qso:=zylo.ToQSO(ptr)
+	qso.SetCall("JA1ZLO")
+	log:=new(zylo.Log)
+	*log=append(*log,*qso) 
+	insert=append(insert,log.Dump(time.Local)...)
+
+	ws, _ := websocket.Dial(url,"",origin)
+	websocket.Message.Send(ws,insert)
+	time.Sleep(2*time.Second)
+	_=ws.Close()
+	
+}
+
+//export zdelete
+func zdelete(ptr uintptr) {
+
+}
+
+func main() {}
