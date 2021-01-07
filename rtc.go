5afef32527d9b43d3960148f69fe40ebac664ff5
jucky154 <chemistry54@docomo.ne.jp>
2021-01-07 13:41:18 +0900

    check rival and register

--- rtc.go
+++ rtc.go
@@ -1,9 +1,9 @@
 /*
  implements the real time contest client software.
  Copyright (C) 2020 JA1ZLO.
- */
+*/
 package main
-​
+
 import (
 	"C"
 	"fmt"
@@ -13,17 +13,39 @@ import (
 	"github.com/nextzlog/zylo"
 	"github.com/recws-org/recws"
 	"gopkg.in/go-toast/toast.v1"
+	"encoding/json"
+	"strconv"
+	"sort"
 )
-​
+
 var (
 	ws = recws.RecConn {
 		KeepAliveTimeout: 30 * time.Second,
 	}
-	url = "wss://realtime.allja1.org/agent/a6ceda1d-517a-404b-92d5-2b7bfb9bc73b"
-	call = "JA1ZLO"
+	url string
 	mainWindow *winc.Form
+	ls[99] *winc.ListView
+	ls_rank *winc.ListView
+	panel[99] *winc.Panel
+	keys[99] string
+	tabs *winc.TabView
+	first int
+	btnfirst int
+	sections map[string] ([]Station)
 )
-​
+
+
+type Station struct { 
+	CALL   string       `json:"call"`
+	SCORE  int          `json:"score"`
+	TOTAL  int          `json:"total"`
+}
+
+type ByTOTAL []Station
+func (a ByTOTAL) Len() int          {return len(a)}
+func (a ByTOTAL) Less(i,j int) bool {return a[i].TOTAL<a[j].TOTAL}
+func (a ByTOTAL) Swap(i,j int)      {a[i],a[j]=a[i],a[i]}
+
 func notify(msg string) {
 	toast := toast.Notification {
 		AppID: "ZyLO",
@@ -32,134 +54,106 @@ func notify(msg string) {
 	}
 	toast.Push()
 }
-​
+
 type Item struct {
 	T       []string
 	checked bool
 }
-​
+
+
 func (item Item) Text() []string    { return item.T }
 func (item *Item) SetText(s string) { item.T[0] = s }
-​
+
 func (item Item) Checked() bool            { return item.checked }
 func (item *Item) SetChecked(checked bool) { item.checked = checked }
 func (item Item) ImageIndex() int          { return 0 }
-​
-​
+
+
 //export zlaunch
 func zlaunch(cfg string) {
-	ws.Dial(url, nil)
-	err := ws.GetDialError()
-	if err != nil {
-		notify(err.Error())
-	} else {
-		notify(fmt.Sprintf("successfully connected to %s", url))
-		go onmessage(call)
-	}
-	mainWindow := winc.NewForm(nil)
-	dock := winc.NewSimpleDock(mainWindow)
-​
-	mainWindow.SetSize(700, 600)
-	mainWindow.SetText("Controls Demo")
-​
-	
-​
-	// --- Tabs
-	tabs := winc.NewTabView(mainWindow)
-	panel1 := tabs.AddPanel("single op")
-	panel2 := tabs.AddPanel("multi op")
-	
-​
-​
-	ls := winc.NewListView(panel1)
-	ls.EnableEditLabels(false)
-	ls.AddColumn("prize", 120)
-	ls.AddColumn("callsign", 120)
-	ls.AddColumn("point", 120)
-	ls.AddColumn("multi", 120)
-	ls.AddColumn("total point", 120)
-	ls.SetPos(10, 180)
-	p1 := &Item{[]string{"First", "JA1ZLO","10","10","100"}, true}
-	ls.AddItem(p1)
-	p2 := &Item{[]string{"Second", "JA1YWX","9","9","81"}, true}
-	ls.AddItem(p2)
-	p3 := &Item{[]string{"Third ", "JA1ZGP","1","1","1"}, true}
-	ls.AddItem(p3)
-​
-	ls2 := winc.NewListView(panel2)
-	ls2.EnableEditLabels(false)
-	ls2.AddColumn("prize", 120)
-	ls2.AddColumn("callsign", 120)
-	ls2.AddColumn("point", 120)
-	ls2.AddColumn("multi", 120)
-	ls2.AddColumn("total point", 120)
-	ls2.SetPos(10, 180)
-	p4 := &Item{[]string{"First", "JA1ZLO","10","10","100"}, true}
-	ls2.AddItem(p4)
-	p5 := &Item{[]string{"Second", "JA1YWX","9","9","81"}, true}
-	ls2.AddItem(p5)
-	p6 := &Item{[]string{"Third ", "JA1RL","1","1","1"}, true}
-	ls2.AddItem(p6)
-	
-​
-	// --- Dock
-	dock1 := winc.NewSimpleDock(panel1)
-	dock1.Dock(ls, winc.Fill)
-​
-	dock2 := winc.NewSimpleDock(panel2)
-	dock2.Dock(ls2, winc.Fill)
-	tabs.SetCurrent(0)
-​
-	dock.Dock(tabs, winc.Top)           // tabs should prefer docking at the top
-	dock.Dock(tabs.Panels(), winc.Fill) // tab panels dock just below tabs and fill area
-​
-	mainWindow.Center()
-	mainWindow.Show()
+	subWindow := winc.NewForm(nil)
+	subWindow.SetSize(400, 300)
+	subWindow.SetText("Registration")
+	edt := winc.NewEdit(subWindow)
+	edt.SetPos(10, 20)
+	// Most Controls have default size unless SetSize is called.
+	edt.SetText("wss://realtime.allja1.org/agent/")
+	btn := winc.NewPushButton(subWindow)
+	btn.SetText("Register")
+	btn.SetPos(40, 50)
+	btn.SetSize(100, 40)
+	btn.OnClick().Bind(func(e *winc.Event) {
+		url:=edt.ControlBase.Text()
+		ws.Dial(url, nil)
+		err := ws.GetDialError()
+		if err != nil {
+			notify(err.Error())
+			notify(fmt.Sprintf("register again"))
+		} else {
+			notify(fmt.Sprintf("successfully connected to %s", url))
+			first=1
+			subWindow.Close()
+		}
+		
+	})
+	subWindow.Center()
+	subWindow.Show()
 }
-​
+
+
+
 //export zrevise
 func zrevise(ptr uintptr) {
 	qso := zylo.ToQSO(ptr)
 	qso.SetMul1(qso.GetRcvd())
 }
-​
+
 //export zverify
 func zverify(ptr uintptr) (score int) {
 	score = 1;
 	return;
 }
-​
+
 //export zresult
 func zresult(log uintptr) (total int) {
 	total = 0;
 	return;
 }
-​
+
 const (
 	INSERT = 0
 	DELETE = 1
 )
-​
+
 //export zinsert
 func zinsert(ptr uintptr) {
 	qso := zylo.ToQSO(ptr)
 	sendQSO(INSERT, qso)
 	notify(fmt.Sprintf("append QSO with %s", qso.GetCall()))
+	if first==1{
+		_,data,err := ws.ReadMessage()
+		if err == nil {	
+			json.Unmarshal(data, &sections)
+			makemainWindow()
+			go onmessage()
+			first=0
+		}
+	}
 }
-​
+
 //export zdelete
 func zdelete(ptr uintptr) {
 	qso := zylo.ToQSO(ptr)
 	sendQSO(DELETE, qso)
 	notify(fmt.Sprintf("delete QSO with %s", qso.GetCall()))
 }
-​
+
 //export zfinish
 func zfinish() {
 	ws.Close()
 	mainWindow.Close()
 }
-​
+
 func sendQSO(request byte, qso *zylo.QSO) {
 	log := append(*new(zylo.Log), *qso)
 	msg := append([]byte{request}, log.Dump(time.Local)...)
@@ -168,14 +162,133 @@ func sendQSO(request byte, qso *zylo.QSO) {
 		notify(err.Error())
 	}
 }
-​
-func onmessage(call string) {
-	for {
-		_,msg,err := ws.ReadMessage()
-		if err == nil {
-			notify(string(msg))
+
+func onmessage() {
+	for{
+		_,data,err := ws.ReadMessage()
+		if err == nil {	
+			json.Unmarshal(data, &sections)
+			reload(sections)
 		}
 	}
 }
-​
+
+func makemainWindow(){
+	// --- Make Window
+	mainWindow = winc.NewForm(nil)
+
+	mainWindow.SetSize(700, 600)
+	mainWindow.SetText("Ranking")
+	dock := winc.NewSimpleDock(mainWindow)
+	tabs = winc.NewTabView(mainWindow)
+	//check rank
+	
+	
+	panel[0] = tabs.AddPanel("your rank")
+	ls_rank := winc.NewListView(panel[0])
+	ls_rank.EnableEditLabels(false)
+	ls_rank.AddColumn("section", 120)
+	ls_rank.AddColumn("rank", 120)
+	ls_rank.AddColumn("call sign", 120)
+	ls_rank.AddColumn("point", 120)
+	ls_rank.AddColumn("score", 120)
+
+	dock1 := winc.NewSimpleDock(panel[0])
+	dock1.Dock(ls_rank, winc.Fill)
+
+	panel[1] = tabs.AddPanel("what is your callsign?")
+	edt := winc.NewEdit(panel[1])
+	edt.SetPos(10, 20)
+	edt.SetText("callsign")
+
+	btn := winc.NewPushButton(panel[1])
+	btn.SetText("check!")
+	btn.SetPos(40, 50)
+	btn.SetSize(100, 40)
+	btnfirst=1
+	btn.OnClick().Bind(func(e *winc.Event) {
+		callsign:=edt.ControlBase.Text()
+		for section_name,section := range sections {
+			sort.Sort(ByTOTAL(section))
+			j:=1
+			for _, station := range section {
+					if station.CALL==callsign{
+					p := &Item{[]string{section_name,strconv.Itoa(j), station.CALL, strconv.Itoa(station.SCORE), strconv.Itoa(station.TOTAL)}, false}
+					ls_rank.AddItem(p)
+				}
+				j=j+1
+			}
+		}	
+		btnfirst=0		
+	})	
+
+	i:=2
+	for section_name,section := range sections {	
+		// --- Tabs
+		panel[i] = tabs.AddPanel(section_name)
+		keys[i]=section_name
+
+		ls[i] = winc.NewListView(panel[i])
+		ls[i].EnableEditLabels(false)
+		ls[i].AddColumn("rank", 120)
+		ls[i].AddColumn("call sign", 120)
+		ls[i].AddColumn("point", 120)
+		ls[i].AddColumn("score", 120)
+		ls[i].SetPos(10, 180)
+			
+		j:=1
+		sort.Sort(ByTOTAL(section))
+		for _, station := range section {
+			p := &Item{[]string{strconv.Itoa(j), station.CALL, strconv.Itoa(station.SCORE), strconv.Itoa(station.TOTAL)}, false}
+			ls[i].AddItem(p)
+			j=j+1
+		}
+
+		// --- Dock(list and tab)
+		dock1 := winc.NewSimpleDock(panel[i])
+		dock1.Dock(ls[i], winc.Fill)
+		i=i+1
+	}
+
+	tabs.SetCurrent(0)
+
+	// --- Dock(window and tabs)
+	dock.Dock(tabs, winc.Top)           
+	dock.Dock(tabs.Panels(), winc.Fill)
+	mainWindow.Center()
+	mainWindow.Show()
+}
+ 
+func reload(sections map[string] ([]Station)){
+	for section_name,section := range sections {
+		//get index number 
+		i:=2
+		for _,_= range sections {
+			if section_name == keys[i]{
+				break
+			}
+			i=i+1
+		}
+
+		//reload
+		//delete ranking
+		ls[i].DeleteAllItems()
+					
+		j:=1
+		sort.Sort(ByTOTAL(section))
+		for _, station := range section {
+			p := &Item{[]string{strconv.Itoa(j), station.CALL, strconv.Itoa(station.SCORE), strconv.Itoa(station.TOTAL)}, false}
+			ls[i].AddItem(p)
+			j=j+1
+		}
+
+		// --- Dock(list and tab)
+		dock1 := winc.NewSimpleDock(panel[i])
+		dock1.Dock(ls[i], winc.Fill)
+	}
+}
+
+
+
+
 func main() {}
